18c32eb0d49f8fe0874cef4dc4877cf6e7e5911e
Marcos Vinicius Araujo da Silva <marcosviniciusjahu@gmail.com>
2024-02-08 12:13:36 -0300

    test: Implementig Unitests

--- src/validators/tag_creator_validator_test.py
+++ src/validators/tag_creator_validator_test.py
@@ -1,5 +1,5 @@
 from .tag_creator_validator import tag_creator_validator
-
+from src.errors.error_types.http_unprocessed_entity import HttpUnprocessedEntityError
 class MockRequest:
     def __init__(self,json) -> None:
         self.json= json
@@ -9,4 +9,10 @@ def test_tag_creator_validator():
     tag_creator_validator(req)
 
 def test_tag_creator_validator_with_error():
-    pass
+    req= MockRequest(json={"product_code":12345})
+    
+    try:
+        tag_creator_validator(req)
+        assert False
+    except Exception as exception:
+        assert isinstance(exception, HttpUnprocessedEntityError)
